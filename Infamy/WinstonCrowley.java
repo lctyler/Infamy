7d55112c909b68412c530597b72f3cf9cf51090a
Drew Troxell <troxellus@gmail.com>
2013-10-28 16:48:17 -0700

    Animooted running

--- Infamy/WinstonCrowley.java
+++ Infamy/WinstonCrowley.java
@@ -8,18 +8,33 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class WinstonCrowley extends British
 {
+    private int runTimer = 0;
+    
     /**
      * Act - do whatever the WinstonCrowley wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-	    if(Greenfoot.mouseClicked(null)){
+        if(Greenfoot.mouseClicked(null)){
             MouseInfo info = Greenfoot.getMouseInfo();
             getWorld().addObject(new Bullet(this, info.getX(), info.getY()), 
                 this.getX() + this.getImage().getWidth()/2 + 10, this.getY() - 10);
         }
-        move();
+        if(move())
+        {
+            runTimer++;
+            if (runTimer==24)
+            {
+                setImage("winston-running-right-4.png");
+                runTimer=0;  
+            }
+            if (runTimer==0) setImage("winston-running-right-1.png");  
+            if (runTimer==8) setImage("winston-running-right-2.png");  
+            if (runTimer==16) setImage("winston-running-right-3.png");
+        }
+        else
+            setImage("man-aiming-right.png");
         d.ExecuteDialogueInteraction();
     }    
 }
