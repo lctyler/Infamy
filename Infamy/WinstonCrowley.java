19c4bfc91dee39ad366b1895ab5e59026a933efa
Dylan <dylanrgraves@gmail.com>
2013-10-28 19:02:05 -0700

    Reloading and fire rate implemented from Winston

--- Infamy/WinstonCrowley.java
+++ Infamy/WinstonCrowley.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.awt.Color;
 /**
  * Write a description of class WinstonCrowley here.
  * 
@@ -11,26 +11,44 @@ public class WinstonCrowley extends British
     private int runTimer = 0;
     private boolean loaded = true;
     private int lastShot = 0;
+    private int shootSpeed = 20;
+    private int reloadClock = 0;
     private int reload = 100;
+    private int magazineRemaining = 5;
+    Dialogue reloadDialogue = new Dialogue();
     /**
      * Act - do whatever the WinstonCrowley wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        if (Greenfoot.mouseClicked(null) && loaded) {
-            loaded = false;
+        if (Greenfoot.mouseClicked(null) && loaded && lastShot / shootSpeed > 0) {
+            if(--magazineRemaining == 0){
+               reloadClock = 0;
+               loaded = false;
+            }
             lastShot = 0;
             MouseInfo info = Greenfoot.getMouseInfo();
             Greenfoot.playSound("gunFire.mp3");
             getWorld().addObject(new Bullet(this, info.getX(), info.getY()), 
             this.getX() + this.getImage().getWidth()/2 + 10, this.getY() - 10);
-        }else {
-            if(lastShot / reload > 0) {
+        }else if(!loaded){
+            if(reloadClock / reload > 0) {
                 loaded = true;
+                magazineRemaining = 5;
             } else {
-                lastShot++;
+                reloadClock++;
             }
+        } else {
+            lastShot++;
+        }
+        if(!loaded) {
+             GreenfootImage reloadMessage = new GreenfootImage("Reloading", 30
+                                    , Color.WHITE, new Color(0,0,0,Color.TRANSLUCENT));
+             reloadDialogue.setImage(reloadMessage);
+             this.getWorld().addObject(reloadDialogue, 50, this.getWorld().getHeight() - 50);
+        } else {
+             this.getWorld().removeObject(reloadDialogue);
         }
         if(move())
         {
