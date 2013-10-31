ace39e5f540e6ddea9b7883bb9f5006108c97387
Michael Sanchez <michael.sanchez@mindbodyonline.com>
2013-10-30 18:53:27 -0700

    Added landmine class for real

---
+++ Infamy/Landmine.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Landmine here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Landmine extends Actor
+{
+    /**
+     * Act - do whatever the Landmine wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
