652a1a24c59c1f80ac4bbafc0f6cd74daf28b9f7
Michael Sanchez <michael.sanchez@mindbodyonline.com>
2013-10-31 19:24:18 -0700

    Added extreme losing condition with crazy laugh sound

--- Infamy/DefeatWorld.java
+++ Infamy/DefeatWorld.java
@@ -9,15 +9,38 @@ import java.awt.Color;
 public class DefeatWorld extends World
 {
     Dialogue score;
+    Dialogue crazy;
     /**
      * Constructor for objects of class DefeatWorld.
      * 
      */
-    public DefeatWorld(int score)
+    public DefeatWorld(int scor)
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1024, 600, 1); 
-        AddScoreDialogue("Your Score:" + score, 512, 400);
+        AddScoreDialogue("Your Score:" + scor, 512, 400);
+        if (Human.Score < 500) {
+            AddCrazyDialogue();
+            Greenfoot.playSound("crazyLaugh.mp3");
+        }
+    }
+    
+    private void AddCrazyDialogue()
+    {
+        crazy = new Dialogue();
+    
+        GreenfootImage textImage = new GreenfootImage("You went crazy from PTSD...", 48, Color.BLACK, Color.WHITE);
+        //textImage.drawString(message, x, y);
+        
+        GreenfootImage image = new GreenfootImage(textImage.getWidth()+12, textImage.getHeight()+12);  
+        image.setColor(Color.BLACK);  
+        image.fill();  
+        image.setColor(Color.WHITE);  
+        image.fillRect(3, 3, image.getWidth()-6, image.getHeight()-6);  
+        image.drawImage(textImage, 6, 6);
+        
+        crazy.setImage(image);
+        addObject(crazy, 512, 300);
     }
     
     private void AddScoreDialogue(String message, int x, int y)
